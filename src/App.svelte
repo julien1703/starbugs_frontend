924ccbb6b0177700f4d604afa0ab63900f21bd6f
FR4NZP <kidpfisto@gmail.com>
2024-06-23 12:36:46 +0200

    OpenAI connection

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,11 @@
   import { onMount } from "svelte";
   import Router from "svelte-spa-router";
   import { push } from "svelte-spa-router";
-  // import Starsign from './[starsign].svelte';
   import Test from "./test.svelte"; // Importieren Sie die neue Seite
   import Three from "./three.svelte"; // Importieren Sie die neue Seite
   import Starsign from "./[starsign].svelte";
 
   const routes = {
-    // '/:starsign': Starsign,
     "/test": Test, // Neue Route hinzufügen
     "/": Three,
     "/:starsign": Starsign,
@@ -16,9 +14,5 @@
 </script>
 
 <main>
-  <h1>Hallo alle zusammen</h1>
-
-  <button on:click={() => push("/test")}>Test</button>
-  <!-- Neuer Button -->
   <Router {routes} />
 </main>
--- src/[starsign].svelte
+++ src/[starsign].svelte
@@ -1,16 +1,26 @@
 <script>
   import { push } from "svelte-spa-router";
   import { onMount } from "svelte";
+  import axios from "axios";
 
   let starsign = "";
+  let apiKey = "";
 
-  let hashFragment;
-
-  onMount(() => {
-    hashFragment = window.location.hash.substring(1);
+  onMount(async () => {
+    const hashFragment = window.location.hash.substring(1);
     hashFragment.split("/").forEach((fragment) => {
       starsign = fragment;
     });
+
+    // API-Schlüssel vom Backend abrufen
+    try {
+      const response = await axios.get('https://api.julien-offray.de/get-api-key');
+      apiKey = response.data.apiKey;
+      console.log("API Key:", apiKey);
+    } catch (error) {
+      console.error("Fehler beim Abrufen des API-Schlüssels:", error);
+    }
+
     console.log(hashFragment);
     console.log(starsign);
   });
