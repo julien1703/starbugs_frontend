92e008bf6d1f48f2873d07d0f50e2873d1af67e6
Julien1703 <128695505+Julien1703@users.noreply.github.com>
2024-07-02 18:03:59 +0200

    alter code

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,11 @@
   import Router from "svelte-spa-router";
   import { push } from "svelte-spa-router";
   import Three from "./three.svelte"; 
-  import Chat from "./chat.svelte"; 
   import Starsign from "./[starsign].svelte";
 
   const routes = {
     "/": Three,
     "/:starsign": Starsign,
-    "/chat": Chat,
   };
 </script>
 
