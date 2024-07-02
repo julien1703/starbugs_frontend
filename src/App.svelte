9b089ae105d5870f72fa0b448dbab62a79faf926
Julien1703 <128695505+Julien1703@users.noreply.github.com>
2024-07-02 16:27:33 +0200

    test datei rausgemacht

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,10 @@
   import { onMount } from "svelte";
   import Router from "svelte-spa-router";
   import { push } from "svelte-spa-router";
-  import Test from "./test.svelte"; // Importieren Sie die neue Seite
   import Three from "./three.svelte"; // Importieren Sie die neue Seite
   import Starsign from "./[starsign].svelte";
 
   const routes = {
-    "/test": Test, // Neue Route hinzufügen
     "/": Three,
     "/:starsign": Starsign,
   };
