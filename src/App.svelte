ca639d94939ae3b727b860727b1501f2e42db261
FR4NZP <kidpfisto@gmail.com>
2024-07-02 17:34:22 +0200

    chat seite hinzugefügt

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,14 @@
   import { onMount } from "svelte";
   import Router from "svelte-spa-router";
   import { push } from "svelte-spa-router";
-  import Three from "./three.svelte"; // Importieren Sie die neue Seite
+  import Three from "./three.svelte"; 
+  import Chat from "./chat.svelte"; 
   import Starsign from "./[starsign].svelte";
 
   const routes = {
     "/": Three,
     "/:starsign": Starsign,
+    "/chat": Chat,
   };
 </script>
 
